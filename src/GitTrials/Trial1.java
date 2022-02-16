ac1b73a2786249be80d7045c595df8e8b36a5ad0
bhagya <bhagya.mahalle@gmail.com>
2022-02-16 23:43:56 +0530

    this is commit from User1

---
+++ src/GitTrials/Trial1.java
@@ -0,0 +1,11 @@
+package GitTrials;
+
+public class Trial1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("updated repository");
+
+	}
+
+}
