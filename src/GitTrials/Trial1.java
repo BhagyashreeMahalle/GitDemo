49b58187876a69686b509dd04f072ce028623443
bhagya <bhagya.mahalle@gmail.com>
2022-02-16 23:52:20 +0530

    User X commit

--- src/GitTrials/Trial1.java
+++ src/GitTrials/Trial1.java
@@ -5,6 +5,7 @@ public class Trial1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("updated repository");
+		System.out.println("updated by X");
 
 	}
 
